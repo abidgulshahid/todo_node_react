e769c1d600dc7e315ebf5ac41e18a2b4e8d8b9ab
Abid Gul Shahid <silencemind55@gmail.com>
2022-11-27 02:09:56 +0500

    fix delete bug

--- server/api/services/delete_todo.js
+++ server/api/services/delete_todo.js
@@ -3,7 +3,7 @@ const todo_table = require("../utils/schema");
 const router = express.Router();
 
 exports.delete_todo = async  (request, response) => {
-    const delete_todo = await todo_table.findOneAndDelete(request.params.id);
+    const delete_todo = await todo_table.findByIdAndDelete(request.params.id);
     try {
         response.status(200).send(delete_todo)
         console.log("Todo Deleted")
